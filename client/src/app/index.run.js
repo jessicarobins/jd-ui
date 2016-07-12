cfdb664525e51921a6ea87cd7371a48e77ed2c32
jrobins <jessrrobins@gmail.com>
2016-07-12 02:03:07 +0000

    deleted some stuff, moved other stuff around

--- client/src/app/index.run.js
+++ client/src/app/index.run.js
@@ -1,4 +1,25 @@
-export function runBlock ($log) {
+export function runBlock ($log, $rootScope, $location) {
   'ngInject';
+  
+  $rootScope.$on('$destroy', 
+      $rootScope.$on('auth:login-success', function() {
+        $location.path('/');
+        alert('old user logged in through oauth');
+    }));
+    $rootScope.$on('$destroy', 
+      $rootScope.$on('auth:oauth-registration', function(ev, user) {
+        $location.path('/');
+        alert('new user registered through oauth:' + user.email);
+    }));
+    $rootScope.$on('$destroy', 
+      $rootScope.$on('auth:login-error', function(ev, reason) {
+          $location.path('/');
+          alert('auth failed because', reason.errors[0]);
+    }));
+    $rootScope.$on('$destroy', 
+      $rootScope.$on('auth:invalid', function(ev, reason) {
+          $location.path('/');
+          alert('auth failed because', reason);
+    }));
   $log.debug('runBlock end');
 }
