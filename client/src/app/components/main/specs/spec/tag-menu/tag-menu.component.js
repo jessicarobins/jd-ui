6bf4c8c956ae41cd0ad8b18f83a4c193d5781579
jrobins <jessrrobins@gmail.com>
2016-07-23 04:23:32 +0000

    it is possible this is a hack but we have checkboxes checked in tag menu

--- client/src/app/components/main/specs/spec/tag-menu/tag-menu.component.js
+++ client/src/app/components/main/specs/spec/tag-menu/tag-menu.component.js
@@ -8,22 +8,46 @@ module.component('tagMenu', {
     controller: function($specs, $tagtypes) {
             
        var self = this;
+       self.formData = {};
+       self.formData.tagTypes = [];
   
        self.openTagMenu = function($mdOpenMenu, ev) {
             $tagtypes.getTagTypes().then( function(response){
                 self.tagTypesByGroup = response.byGroup;
+                formatTagData();
                 $mdOpenMenu(ev);
-                console.log('tag types by group', self.tagTypesByGroup);
             });
         };
        
         self.toggleTag = function(tagTypeId){
-            addTag(tagTypeId);
+            if(self.hasTag(tagTypeId)){
+                
+            }
+            else {
+                addTag(tagTypeId);
+            }
         };
         
         function addTag(tagTypeId){
             $specs.addTag(self.spec, tagTypeId).then(function (tag){
                self.tags.push(tag); 
+            });
+        }
+        
+        self.hasTag = function(tagTypeId){
+            var i=0, len=self.tags.length;
+            for (; i<len; i++) {
+                if (+self.tags[i].tag_type.id == +tagTypeId) {
+                  self.formData.tagTypes[tagTypeId] = true;
+                  return self.tags[i];
+                }
+            }
+            return null;  
+        };
+        
+        function formatTagData(){
+            self.tags.forEach( function(tag){
+                self.formData.tagTypes[tag.tag_type_id] = true;
             });
         }
     }
