f77aeff7e84377db95afc842e1692d064c03129d
jrobins <jessrrobins@gmail.com>
2016-08-11 01:50:08 +0000

    comment menu

---
+++ client/src/app/components/main/specs/spec/comment-menu/comment-menu.component.js
@@ -0,0 +1,60 @@
+module.component('commentMenu', {
+    require: {
+        parent: '^^spec'
+    },
+    bindings: {
+        spec: '<'
+    },
+    templateUrl: 'app/components/main/specs/spec/comment-menu/comment-menu.template.html',
+    controller: function($api) {
+        
+        var self = this;
+         
+        self.$onInit = function(){
+            self.spec.grouped_comments_json.false = self.spec.grouped_comments_json.false || [];
+            self.spec.grouped_comments_json.true = self.spec.grouped_comments_json.true || [];  
+        };
+        
+       self.openCommentMenu = function($mdOpenMenu, ev) {
+            $mdOpenMenu(ev);
+        };
+        
+        self.hasComments = function(){
+            return self.spec.grouped_comments_json.false.length ||
+                self.spec.grouped_comments_json.true.length;
+        };
+        
+        self.formatTime = function(comment){
+            var time = moment(comment.created_at).format('M/D/YY h:mm a');
+            return time;
+        };
+        
+        self.add = function(){
+            $api.request({
+                url: '/comments',
+                method: 'POST',
+                data: {
+                    comment: {
+                        text: self.text,
+                        spec_id: self.spec.id
+                    }
+                }
+            }).then( function(response){
+               self.spec.grouped_comments_json.false.push(response);
+               self.spec.open_comments_count += 1;
+               self.text = '';
+            });
+        };
+        
+        self.resolve = function(comment){
+            $api.request({
+                url: '/comments/' + comment.id + '/resolve',
+                method: 'PUT'
+            }).then(function(){
+                self.spec.grouped_comments_json.true.push(comment);
+                _.pull(self.spec.grouped_comments_json.false, comment);
+                self.spec.open_comments_count -= 1;
+            });
+        };
+    }
+});
