98fe7903121f525e4e750814c2a8e954c4ae3df0
jrobins <jessrrobins@gmail.com>
2016-09-13 23:27:30 +0000

    menu is now stateless

--- src/app/components/main/specs/spec/react-menu/menu.jsx
+++ src/app/components/main/specs/spec/react-menu/menu.jsx
@@ -5,15 +5,14 @@ import MenuItem from 'material-ui/MenuItem';
 import IconButton from 'material-ui/IconButton/IconButton';
 import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
 
-var MenuComponent = React.createClass ({
+const MenuComponent = ({menuOptions, spec}) => {
   
-  getMenuItems: function() {
-    const spec = this.props.spec;
+  const getMenuItems = () => {
     let opts;
     if(spec.bookmarked){
-      opts = _.reject(this.props.menuOptions, {name: 'bookmark'});
+      opts = _.reject(menuOptions, {name: 'bookmark'});
     } else {
-      opts = _.reject(this.props.menuOptions, {name: 'unbookmark'});
+      opts = _.reject(menuOptions, {name: 'unbookmark'});
     }
     return opts.map(function(menuItem){
       return (
@@ -24,22 +23,19 @@ var MenuComponent = React.createClass ({
           leftIcon={<FontIcon className="material-icons">{menuItem.icon}</FontIcon>}/>
       );
     });
-  },
+  };
   
-  render: function() {
-    
-    return (
-      <IconMenu
-        iconButtonElement={<IconButton><MoreVertIcon /></IconButton>}
-        anchorOrigin={{horizontal: 'right', vertical: 'top'}}
-        targetOrigin={{horizontal: 'right', vertical: 'top'}}
-        maxHeight={272}
-      >
-        {this.getMenuItems()}
-      </IconMenu>
-    );
-  }
-});
+  return (
+    <IconMenu
+      iconButtonElement={<IconButton><MoreVertIcon /></IconButton>}
+      anchorOrigin={{horizontal: 'right', vertical: 'top'}}
+      targetOrigin={{horizontal: 'right', vertical: 'top'}}
+      maxHeight={272}
+    >
+      {getMenuItems()}
+    </IconMenu>
+  );
+};
 
 
 
